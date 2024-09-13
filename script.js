a2b4adf1d706aea14f9ebd4d263d11407bf0d340
CGbhakt420 <sanchit.kr007@gmail.com>
2024-09-13 19:27:11 +0530

    created object for the entered book details in the form

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-    
-  // do stuff here
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").checked;
+  let newBook = new Book(title, author, pages, read);
+  console.log(newBook); // This will log the new book object
+  myLibrary.push(newBook); // Optionally, you can add the book to the library array
 }
 
 let newBookBtn = document.querySelector(".new-book");
-newBookBtn.addEventListener("click", function(){
-    let newBookform = document.querySelector("#new-book-entry");
-    newBookform.style.display = "flex";
-})
+newBookBtn.addEventListener("click", function() {
+  let newBookForm = document.querySelector("#new-book-entry");
+  newBookForm.style.display = "flex";
+});
+
+// Attach the event listener to the form, not the button
+let newBookForm = document.querySelector("#new-book-entry");
+newBookForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Correctly call the preventDefault method
+  addBookToLibrary();
+});
