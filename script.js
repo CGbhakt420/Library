55bb415f438beb807438c016e47e4f176860cef0
CGbhakt420 <sanchit.kr007@gmail.com>
2024-09-13 21:11:41 +0530

    added cards ti be displayed for every book submitted through the form

--- script.js
+++ script.js
@@ -7,14 +7,42 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+
 function addBookToLibrary() {
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
   let pages = document.querySelector("#pages").value;
   let read = document.querySelector("#read").checked;
   let newBook = new Book(title, author, pages, read);
-  console.log(newBook); // This will log the new book object
   myLibrary.push(newBook); // Optionally, you can add the book to the library array
+  render();
+}
+
+function render(){
+    let libraryEl = document.querySelector("#library");
+    libraryEl.innerHTML = "";
+    for(let i=0; i<myLibrary.length; i++){
+        let book = myLibrary[i];
+        let bookEl = document.createElement("div");
+        bookEl.setAttribute("class", "book-card");
+        bookEl.innerHTML=`
+            <div class = "class-header">
+                <h3 class = "title">${book.title}</h3>
+                <h5 class = "author">${book.author}</h5>
+            </div>
+            <div class = "card-body">
+                <p>${book.pages} pages</p>
+                <p class="read-status">${book.read ? "Read" : "Not read yet"}</p>
+                <button class = "remove-button" onclick = "removeBook(${i})">Remove</button>
+            </div>
+            `;  
+        libraryEl.appendChild(bookEl);
+    }
+}
+
+function removeBook(index){
+    myLibrary.splice(index,1);
+    render();
 }
 
 let newBookBtn = document.querySelector(".new-book");
